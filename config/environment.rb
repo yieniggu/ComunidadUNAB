9807d203ab4bb11fa4fa2134753a9b833132b488
yieniggu <bastianandresxb@gmail.com>
2019-10-12 00:20:08 -0300

    Añadido host de heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+    config.hosts.clear
+end
+
+config.hosts << "comunidad-unab.herokuapp.com"
